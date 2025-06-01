d59153cb10a0e5202dbaf727b70def9e874fd730
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-06-01 11:37:10 +0100

    voye

---
+++ count_letters.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    char str[100];
+    scanf("%s", str);
+    int count = 0;
+    int voyelles = 0;
+    int consonnes = 0;
+    while (str[count] != '\0') {
+        char c = str[count];
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                voyelles++;
+            } else {
+                consonnes++;
+            }
+        }
+        count++;
+    }
+    printf("Voyelles : %d\n", voyelles);
+    printf("Consonnes : %d\n", consonnes);
+}
