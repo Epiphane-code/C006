812342860df600dc6e6094a0fd4be34717f40c35
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-06-01 23:23:54 +0100

    lo

--- count_letters.c
+++ count_letters.c
@@ -2,25 +2,17 @@
 int main() {
     char str[100];                                   // declaration d'un tableau de 100 caracteres
     int i, voyelles = 0, consonnes = 0;         // declaration des variables pour le comptage
-    char c;
-    scanf("%99[^\n]", str);                            // lecture de la chaine de caracteres
-    for (i = 0; str[i] != '\0'; i++) {                // boucle pour parcourir la chaine
-        c = str[i];
-        if (c >= 'a' && c <= 'z') {                   // si c est une lettre minuscule
-            c -= 'a';                                 // conversion en index de 0 a 25
-            if (c == 0 || c == 4 || c == 8 || c == 14 || c == 20) {
-                voyelles++;                            // incrementer le compteur de voyelles
+    char c = ' ';                                 // initialisation de la variable c
+    while (c != '\n'){                        // lecture d'un caractere
+        if (c >= 'a' && c <= 'z') {              // si le caractere est une lettre minuscule
+            str[i++] = c;                         // on l'ajoute au tableau
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y') {
+                voyelles++;                        // on incremente le compteur de voyelles
             } else {
-                consonnes++;                           // sinon, incrementer le compteur de consonnes
-            }
-        } else if (c >= 'A' && c <= 'Z') {            // si c est une lettre majuscule
-            c -= 'A';                                 // conversion en index de 0 a 25
-            if (c == 0 || c == 4 || c == 8 || c == 14 || c == 20) {
-                voyelles++;                            // incrementer le compteur de voyelles
-            } else {
-                consonnes++;                           // sinon, incrementer le compteur de consonnes
+                consonnes++;                       // sinon, on incremente le compteur de consonnes
             }
         }
+        scanf("%c", &c);                          // on lit le caractere suivant
     }
     printf("Voyelles : %d\n", voyelles);
     printf("Consonnes : %d\n", consonnes);
