b42f5a0739551c9eaf7f5aa64c375e313998ddf4
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-06-01 14:31:20 +0100

    co

--- string_length.c
+++ string_length.c
@@ -3,11 +3,12 @@ int main() {
     char str[100];
     fgets(str, sizeof(str), stdin);
     int len = 0;
-    int count = 0;
+    int count = -1;
     while (str[len] != '\0') {
         if (str[len] != ' ') {
             count++;
         }
+        len++;
     }
     printf("Longueur de la chaîne : %d\n", count);
     return 0;
