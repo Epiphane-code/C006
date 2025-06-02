1875acfbfb8ebf2efb973332029b51e069508e18
Epiphane-code <omarepiphanefarouk@gmail.com>
2025-06-02 10:30:46 +0100

    lo

--- count_vc.c
+++ count_vc.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-int main() {
-    // Compte le nombre de voyelles et de consonnes dans une phrase
-    int n; 
+int main(){
     int voyelle = 0;
     int consonne = 0;
     char c;
     scanf("%c", &c);
-    while (c != '\n'){
-        if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z' )){
-            if(c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U' || c == 'y' || c == 'Y' ){
-                voyelle++;
-            }
-            else consonne++;
-
+    while (c != '\n') {
+        if (c >= 'a' && c <= 'z') {
+            c = c - 32; // Convertit en majuscule
         }
-        // On lit le caractère suivant
+        if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y') {
+            voyelle++;
+        } else if ((c >= 'B' && c <= 'Z') && !(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y')) {
+            consonne++;
+        }
         scanf("%c", &c);
+    }    
 
-    }
     
     printf("Voyelles : %d\n", voyelle);
     printf("Consonnes : %d\n", consonne);
